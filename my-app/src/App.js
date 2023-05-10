54711e804267ad19b756db84198b70e98d4e1afb
ezea <ezequierjaraepet20@gmail.com>
2023-05-10 00:47:19 -0300

    Creo Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,12 @@
 
 import './App.css';
 import React from 'react';
-import Login from './Login';
-
-const App = () => {
-  return <Login />;
-};
-
 
 function App() {
   return (
-    <div className="App">
-     <authProvider>
-      <routes>
-        <reute path="/Login" element={<Login/>}/>
-        <route 
-        path="/" 
-        element={
-        <protectedRoute>
-          <home/>
-        </protectedRoute>
-      }
-      />
-      <route path= "/Register" element={<Register/>}/>
-      </routes>
-     </authProvider>
-    </div>
+   
+   <Navbar/>
+   
   );
 }
 
---
+++ my-app/src/componentes/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const NavBar = () => {
+    return (
+        <NavContainer>
+            
+
+        </NavContainer>
+    );
+};
+
+const NavContainer = styled.nav `
+
+`
+
+export default NavBar;
