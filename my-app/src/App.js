adfe0aecab1f0fa6b34790369a09918915b3ea03
ezea <ezequierjaraepet20@gmail.com>
2023-05-10 00:19:25 -0300

    Modifico y borro login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 
+import './App.css';
 import React from 'react';
 import Login from './Login';
 
@@ -6,4 +7,27 @@ const App = () => {
   return <Login />;
 };
 
+
+function App() {
+  return (
+    <div className="App">
+     <authProvider>
+      <routes>
+        <reute path="/Login" element={<Login/>}/>
+        <route 
+        path="/" 
+        element={
+        <protectedRoute>
+          <home/>
+        </protectedRoute>
+      }
+      />
+      <route path= "/Register" element={<Register/>}/>
+      </routes>
+     </authProvider>
+    </div>
+  );
+}
+
 export default App;
+
