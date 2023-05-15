fd096aa00d7b0ff4c3d0b9b170605eef71e97e75
ezea <ezequierjaraepet20@gmail.com>
2023-05-15 15:32:57 -0300

    Boton De comienzo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SettingsButton from './SettingsButton';
+import StartMessage from './StartMessage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowMessage(!showMessage); // Invierte el valor actual de showMessage
+  };
+
+  const containerStyle = {
+    position:'relative' ,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundImage: `url("https://www.rionegro.com.ar/wp-content/uploads/2023/04/museo-1.jpg")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',// Opacidad del fondo
+    filter: 'brightness(80%)',
+  };
+  const overlayStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    zIndex: -1, // Fondo negro semitransparente
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      {showMessage && <StartMessage />}
+      <SettingsButton onClick={handleButtonClick} />
+      <div style={overlayStyle}></div>
+
     </div>
+    
   );
 }
 
---
+++ my-app/src/SettingsButton.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+const SettingsButton = ({ onClick }) => {
+  return (
+    <button className="btn btn-outline-danger" onClick={onClick}>
+     comenzar
+    </button>
+  );
+};
+
+export default SettingsButton;
