b49a5210a49cddad0567ae9db888c03d26f494a1
ezea <ezequierjaraepet20@gmail.com>
2023-08-09 04:01:14 -0300

    Nav y fondo nuevo

--- mnba-app/src/components/Header.js
+++ mnba-app/src/components/Header.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import './headerStyles.css'; // Importa el archivo CSS
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    
+    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#EBEBEB' }}>
       <div className="container d-flex justify-content-center align-items-center">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item"><a className="nav-link" href="#contacto">Contacto</a></li>
-            <li className="nav-item"><a className="nav-link" href="#colecciones">Colecciones</a></li>
-            <a className="navbar-brand" href="#">MNBA</a>
-            <li className="nav-item"><a className="nav-link" href="#artistas">Artistas</a></li>
-            <li className="nav-item"><a className="nav-link" href="#creditos">Créditos</a></li>
-          </ul>
+        <div className="text-center">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item nav-S"><a className="nav-link nav-list" href="#contacto">CONTACTO</a></li>
+              <li className="nav-item nav-separator"><a className="nav-link nav-list" href="#colecciones">COLECCIONES</a></li>
+              <a className="navbar-brand nav-brand" href="#">M N B A</a>
+              <li className="nav-item nav-separator"><a className="nav-link nav-list" href="#artistas">ARTISTAS</a></li>
+              <li className="nav-item nav-separator"><a className="nav-link nav-list" href="#creditos">CRÉDITIOS</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
+  
   );
 };
 
