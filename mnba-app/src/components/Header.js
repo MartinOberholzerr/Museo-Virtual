69b95b0237d80897e1b2e334a08bd461d8bea5be
ezea <ezequierjaraepet20@gmail.com>
2023-08-10 12:14:40 -0300

    Logo en pestaña

--- mnba-app/src/components/Header.js
+++ mnba-app/src/components/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
+
 import './headerStyles.css'; // Importa el archivo CSS
 
 const Header = () => {
   return (
     
-    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#EBEBEB' }}>
+    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#EBEBEE' }}>
       <div className="container d-flex justify-content-center align-items-center">
         <div className="text-center">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse navbar-collapse text-center" id="navbarNav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item nav-S"><a className="nav-link nav-list" href="#contacto">CONTACTO</a></li>
               <li className="nav-item nav-separator"><a className="nav-link nav-list" href="#colecciones">COLECCIONES</a></li>
-              <a className="navbar-brand nav-brand" href="#">M N B A</a>
+              <a className="navbar-brand nav-brand" href="/home" style={{ fontSize: '2rem' }}>M N B A</a>
               <li className="nav-item nav-separator"><a className="nav-link nav-list" href="#artistas">ARTISTAS</a></li>
               <li className="nav-item nav-separator"><a className="nav-link nav-list" href="#creditos">CRÉDITIOS</a></li>
             </ul>
@@ -22,7 +23,6 @@ const Header = () => {
         </div>
       </div>
     </nav>
-  
   );
 };
 
