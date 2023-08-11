f56a6f6f860a7335c92022b7f722866d34be6103
ezea <ezequierjaraepet20@gmail.com>
2023-08-11 13:40:38 -0300

    Añado un color a todas las fuentes

--- mnba-app/src/components/Header.js
+++ mnba-app/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './headerStyles.css';
 
 const Header = () => {
+  const linkStyle = { color: '#695746' }; // Define el color para las fuentes de las listas y MNBA
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#EBEBEE' }}>
       <div className="container d-flex justify-content-center align-items-center">
@@ -16,31 +18,31 @@ const Header = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a className="navbar-brand nav-brand d-lg-none" href="/home" style={{ fontSize: '2rem' }}>
+        <a className="navbar-brand nav-brand d-lg-none" href="/home" style={{ fontSize: '2rem', ...linkStyle }}>
           M N B A
         </a>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto d-flex flex-row">
             <li className="nav-item nav-S">
-              <a className="nav-link nav-list" href="#contacto">
+              <a className="nav-link nav-list" href="#contacto" style={linkStyle}>
                 CONTACTO
               </a>
             </li>
             <li className="nav-item nav-separator">
-              <a className="nav-link nav-list" href="#colecciones">
+              <a className="nav-link nav-list" href="#colecciones" style={linkStyle}>
                 COLECCIONES
               </a>
             </li>
-            <a className="navbar-brand nav-brand d-none d-lg-block" href="/home" style={{ fontSize: '2rem' }}>
+            <a className="navbar-brand nav-brand d-none d-lg-block" href="/home" style={{ fontSize: '2rem', ...linkStyle }}>
               M N B A
             </a>
             <li className="nav-item nav-separator">
-              <a className="nav-link nav-list" href="#artistas">
+              <a className="nav-link nav-list" href="#artistas" style={linkStyle}>
                 ARTISTAS
               </a>
             </li>
             <li className="nav-item nav-separator">
-              <a className="nav-link nav-list" href="#creditos">
+              <a className="nav-link nav-list" href="#creditos" style={linkStyle}>
                 CRÉDITOS
               </a>
             </li>
