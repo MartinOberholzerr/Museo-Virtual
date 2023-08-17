1af4111c27514000b58d44ade4f5328f2fdc781d
ezea <ezequierjaraepet20@gmail.com>
2023-08-16 23:50:53 -0300

    Format selection

--- mnba-app/src/components/Contact.js
+++ mnba-app/src/components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
           <p className="overlay-text">CONTACTANOS <br></br>SOMOS MNBA</p>
           <p className="overlay-subtext">Gracias por visitar nuestra experiencia virtual</p>
         </div>
-        
+
         <img src="https://scontent.faep9-1.fna.fbcdn.net/v/t1.6435-9/53152826_1906868212769969_7942975593665003520_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=nyEfROJyOBkAX9ymATL&_nc_ht=scontent.faep9-1.fna&oh=00_AfCpclDNGsL6usqWfnGdWtA_FFxEfQzg3wDvMAiV-ovT3Q&oe=650500C2" alt="Left" />
         <button className='button'>Contactar</button>
       </div>
