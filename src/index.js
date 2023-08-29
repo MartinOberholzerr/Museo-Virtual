8d02aec5820bdc40044291774c61ca6f28e66ff0
lucasvlg <103854376+lucasvlg@users.noreply.github.com>
2023-08-28 22:46:00 -0300

    Incorporando archivos de configuración de Tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+/* import './index.css'; */
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Footer} from './componentes/Footer';
-import { Nav } from './componentes/Navbar';
+/* import { Nav } from './componentes/Navbar'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Footer />
-    <Nav/>
+{/*     <Nav/> */}
   </React.StrictMode>
 );
   
