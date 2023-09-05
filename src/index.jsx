e54b5ae4f42927e76e177ded3aab69852f315b07
lucasvlg <103854376+lucasvlg@users.noreply.github.com>
2023-09-05 18:52:27 -0300

    Subiendo ultimos cambios a la rama

--- src/index.jsx
+++ src/index.jsx
@@ -10,8 +10,7 @@ import {Card} from './componentes/Card.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
-{/*     <Nav/> */}
+    <Nav/>
   </React.StrictMode>
 );
   
