71bf9d1c72e68ac484c62ec82305d318404ddf2d
lucasvlg <103854376+lucasvlg@users.noreply.github.com>
2023-09-12 03:56:59 -0300

    Posicionando foto1

--- src/components/Creditos.js
+++ src/components/Creditos.js
@@ -12,10 +12,14 @@ const Creditos = () => {
       <Link to='/' className='button-link'>
         <FontAwesomeIcon icon={faArrowLeft} className='icon' />
       </Link>
-
       <div className='container'>
           <div  className='img'>
-          <img src='https://www.cultura.gob.ar/media/uploads/51017288816_92a517b177_k.jpg' alt='card3' className='imagen-rojo' />
+          <img src='https://www.cultura.gob.ar/media/uploads/51017288816_92a517b177_k.jpg' alt='foto1' className='imagen-rojo' />
+          </div>
+      </div>
+      <div className='container'>
+          <div  className='img'>
+          <img src='https://www.cultura.gob.ar/media/uploads/34195397795_8aa2428cfa_c.jpg' alt='foto2' className='imagen-escultura' />
           </div>
       </div>
     </div>
