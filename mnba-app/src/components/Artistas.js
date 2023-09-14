5e1f0ae0f522a7ae3f825585b642d964a44ae7f7
lucasvlg <103854376+lucasvlg@users.noreply.github.com>
2023-09-14 03:51:58 -0300

    Creando componente Artistas.js

---
+++ mnba-app/src/components/Artistas.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import './Artistas.css';
+
+const Artistas = () => {
+  
+  return (
+    <div className="container">
+
+    </div>
+  );
+};
+
+export default Artistas;
