4d87db05dfcc309ae3ccff30a50d3954a85630af
lucasvlg <103854376+lucasvlg@users.noreply.github.com>
2023-09-14 04:26:32 -0300

    Cambiando nombre de las clases

--- mnba-app/src/components/Conection.js
+++ mnba-app/src/components/Conection.js
@@ -32,7 +32,7 @@ const Contact = () => {
             </div>
           </div>
           <div  className='card'>
-          <img src='https://th.bing.com/th/id/OIP.vHMlepemjnRJIL5Asr5WEQHaE7?pid=ImgDet&rs=1' alt='card2' className='card-image2' />
+          <img src='https://th.bing.com/th/id/OIP.vHMlepemjnRJIL5Asr5WEQHaE7?pid=ImgDet&rs=1' alt='card2' className='card-image' />
             <div className='card-content'>
             <h3 className='card-title'>Retrato a la concns</h3>
               <p className='card-description'>En este retrato, el artista ha capturado la introspección y la serenidad de su sujeto. La mirada profunda y pensativa del retratado invita a reflexionar sobre los misterios de la mente humana. La paleta de colores suaves y la técnica detallada destacan la expresión facial.</p>
