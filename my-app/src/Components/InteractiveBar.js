798defbb3417fb4a22c13335ec7880d4726eefda
FrancoGiuliani1 <francogiulianiii@gmail.com>
2023-09-14 03:03:14 -0300

    Funcion para mostar y ocultar barra con boton

--- my-app/src/Components/InteractiveBar.js
+++ my-app/src/Components/InteractiveBar.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import './interactiveBar.css';
+
 function InteractiveBar() {
     const [visible, setVisible] = useState(true);
 
     const toggleVisibility = () => {
         setVisible(!visible);
       };
+
+return(
+    <div className={`interactive-bar ${visible ? 'visible' : 'hidden'}`}> 
+    <button onClick={toggleVisibility}>Mostrar/ocultar barra</button>
+</div>
+);
     
 
 
