8d1c73fcf38390ef49470639fa7b8482b78ababa
lucasvlg <103854376+lucasvlg@users.noreply.github.com>
2023-09-16 22:44:55 -0300

    Agregando últimos cambios en Artistas.js

--- mnba-app/src/components/Artistas.js
+++ mnba-app/src/components/Artistas.js
@@ -17,24 +17,24 @@ const Artistas = () => {
       <div className='centered-container'>
         <div className='card-container'>
           <div  className='card'>
-          <img src='https://www.bellasartes.gob.ar/media/uploads/coleccion/11942.jpg' alt='card3' className='card-image' />
+          <img src='https://upload.wikimedia.org/wikipedia/commons/e/e4/Roberto_Plate_%281%29.jpg' alt='card1' className='card-image' />
             <div className='card-content'>
-            <h3 className='card-title'>América II</h3>
-              <p className='card-description'> Esta pintura captura la belleza serena de un amanecer en el bosque. Los tonos suaves y cálidos del sol naciente iluminan el dosel del bosque, creando una atmósfera tranquila y mágica. Los detalles meticulosos de los árboles y las hojas hacen que esta obra sea una celebración</p>
-            </div>
-          </div>
-          <div className='card'>
-          <img src='https://upload.wikimedia.org/wikipedia/commons/6/6e/Delia_Cancela.jpg' alt='card1' className='card-image' />
-            <div className='card-content'>
-            <h3 className='card-title'>Delia Cancela</h3>
-              <p className='card-description'>Delia Cancela (nacida en 1940 en Buenos Aires, Argentina) es una artista pop y diseñadora de modas argentina. Vivió en Argentina, Nueva York, Londres y Paris, ciudades donde desarrolló exposiciones. Exhibiciones retrospectivas de sus trabajos y colaboraciones con Pablo Mesejean.</p>
+            <h3 className='card-title'>Roberto Plate</h3>
+              <p className='card-description'>Nacimiento:	9 de septiembre de 1940 Buenos Aires, Argentina (83 años)<br/>Nacionalidad: Argentina.<br/>Educación: Academia de Múnich. Ocupación: Pintor y escenógrafo.<br/>Distinciones: Juana de Arco en la hoguera de Arthur Honegger - Bomarzo de Alberto Ginastera. - Muerte en Venecia de Britten.</p>
             </div>
           </div>
           <div  className='card'>
-          <img src='https://upload.wikimedia.org/wikipedia/commons/c/c8/Roberto_Jacoby_en_la_presentaci%C3%B3n_de_%22Imaginaci%C3%B3n_Pol%C3%ADtica_-_Proyecto_Ballena_2023%22.jpg' alt='card2' className='card-image' />
+          <img src='https://upload.wikimedia.org/wikipedia/commons/6/6e/Delia_Cancela.jpg' alt='card2' className='card-image' />
             <div className='card-content'>
-            <h3 className='card-title'>Roberto Jacoby</h3>
-              <p className='card-description'>Nacimiento: 29 de julio de 1944. Buenos Aires - Argentina (79 años).<br/>Nacionalidad: argentina<br/>Ocupación:	Sociólogo, artista, letrista<br/>Distinciones: Beca Guggenheim - Primer Manifiesto del Arte y Los Medios. - Premio Nacional a la Trayectoria Artística (2018)</p>
+            <h3 className='card-title'>Delia Cancela</h3>
+              <p className='card-description'>Nacimiento:	nacida en 1940 en Buenos Aires, Argentina(83 años)<br/>Nacionalidad: Argentina.<br/>Ocupación: artista pop y diseñadora de modas.<br/>Distinciones: Delia Cancela -s Retrospectiva - Pablo&Delia - The London Years - Delia Cancela: Un artista en la moda.</p>
+            </div>
+          </div>
+          <div  className='card'>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Luis_Felipe_No%C3%A9_-_Inauguraci%C3%B3n_de_la_muestra_%E2%80%9CErnesto_Deira._Identificaciones%22.jpg' alt='card3' className='card-image' />
+            <div className='card-content'>
+            <h3 className='card-title'>Luis Felipe Noé</h3>
+              <p className='card-description'>Nacimiento: 26 de mayo de 1933 Buenos Aires, Argentina (90 años)<br/>Nacionalidad: Argentina.<br/>Ocupación: Artista (Pintura de personaje y arte abstracto), escritor, pintor y profesor.<br/>Área: Arte.<br/>Distinciones: Beca Guggenheim - Premio Nacional a la Trayectoria Artística (2019).</p>
             </div>
           </div>
         </div>
