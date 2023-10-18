b7653dae3396f99240553dd653e24a78a18f2f19
EzequielJara <ezequierjaraepet20@gmail.com>
2023-10-18 10:45:41 -0300

    Cambio a alert

--- mnba-app/src/components/Login/Login.jsx
+++ mnba-app/src/components/Login/Login.jsx
@@ -4,11 +4,6 @@ import './Login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { useNavigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
-
-import 'react-toastify/dist/ReactToastify.css';
-
 import { getAuth, signInWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 const auth = getAuth(appFirebase);
 function Login(props) {
@@ -26,9 +21,9 @@ function Login(props) {
         } catch (error) {
             // Comprueba el código de error
             if (error.code === 'auth/wrong-password') {
-                toast.error('La contraseña es incorrecta');
+                alert('La contraseña es incorrecta');
             } else if (error.code === 'auth/user-not-found') {
-                toast.error('El correo es incorrecto');
+                alert('El correo es incorrecto');
             } else {
                 console.log(error.message);
             }
@@ -74,17 +69,7 @@ function Login(props) {
         </button>  
 
       </div>
-      <ToastContainer 
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
+      
       </div>
     );
 }
