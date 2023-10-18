2413ab76ef7165fc4fb8357c09650a5a6b02e9cd
EzequielJara <ezequierjaraepet20@gmail.com>
2023-10-18 10:43:19 -0300

    Añado un validor de contraseña y email

--- mnba-app/src/components/Login/Login.jsx
+++ mnba-app/src/components/Login/Login.jsx
@@ -4,33 +4,37 @@ import './Login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 import { getAuth, signInWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 const auth = getAuth(appFirebase);
 function Login(props) {
     const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
-    const [password, setPassword] = useState("");
-  
-  
-    const togglePasswordVisibility = () => {
-      setShowPassword(!showPassword);
-    };
-  
     const functAutenticacion = async (e) =>{  
         e.preventDefault();
         const correo = e.target.email.value;
         const contraseña = e.target.password.value;
          
-          try {
+        try {
             await signInWithEmailAndPassword(auth, correo, contraseña);
             navigate("/");
             console.log("Iniciado sesion con exito!!")
-          } catch (error) {
-            alert ("El correo es incorrecto");
-          }
-        
+        } catch (error) {
+            // Comprueba el código de error
+            if (error.code === 'auth/wrong-password') {
+                toast.error('La contraseña es incorrecta');
+            } else if (error.code === 'auth/user-not-found') {
+                toast.error('El correo es incorrecto');
+            } else {
+                console.log(error.message);
+            }
+        }
     }
+   
   
     const signInWithGoogle = () => {
       const provider = new GoogleAuthProvider();
@@ -70,6 +74,17 @@ function Login(props) {
         </button>  
 
       </div>
+      <ToastContainer 
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
       </div>
     );
 }
