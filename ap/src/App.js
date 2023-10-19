1f4ab7ceebb52d9366e162cf0f57eab3e5787925
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-10-19 19:44:36 -0300

    changes

--- ap/src/App.js
+++ ap/src/App.js
@@ -16,21 +16,5 @@ function App (){
   )
 }
 
-document.getElementById('izquierda').addEventListener('click', function() {
-  const iframe1 = document.getElementById('iframe1');
-  const iframe2 = document.getElementById('iframe2');
-  
-  iframe1.style.display = 'block';
-  iframe2.style.display = 'none';
-});
-
-document.getElementById('derecha').addEventListener('click', function() {
-  const iframe1 = document.getElementById('iframe1');
-  const iframe2 = document.getElementById('iframe2');
-  
-  iframe1.style.display = 'none';
-  iframe2.style.display = 'block';
-});
-
 
 export default App;
--- ap/src/components/move.js
+++ ap/src/components/move.js
@@ -9,4 +9,22 @@ function Moving(){
     </div>
 )}
 
+document.getElementById('izquierda').addEventListener('click', function() {
+  const iframe1 = document.getElementById('iframe1');
+  const iframe2 = document.getElementById('iframe2');
+  
+  iframe1.style.display = 'block';
+  iframe2.style.display = 'none';
+});
+
+document.getElementById('derecha').addEventListener('click', function() {
+  const iframe1 = document.getElementById('iframe1');
+  const iframe2 = document.getElementById('iframe2');
+  
+  iframe1.style.display = 'none';
+  iframe2.style.display = 'block';
+});
+
+
+  
 export default Moving
