3c8159c0002caa93c0d699702390e479fe59f830
EzequielJara <ezequierjaraepet20@gmail.com>
2023-10-18 21:57:32 -0300

    Agrego GitHub al login con logica y estilos

--- mnba-app/src/components/Login/Login.jsx
+++ mnba-app/src/components/Login/Login.jsx
@@ -2,86 +2,88 @@ import React, { useState } from 'react';
 import appFirebase from '../../firebase.js'
 import './Login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
 import { getAuth, signInWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 const auth = getAuth(appFirebase);
 function Login(props) {
-    const navigate = useNavigate();
-    const [showPassword, setShowPassword] = useState(false);
-    
-  
-  
-    const togglePasswordVisibility = () => {
-      setShowPassword(!showPassword);
-    };
-  
-    const functAutenticacion = async (e) =>{  
-        e.preventDefault();
-        const correo = e.target.email.value;
-        const contraseña = e.target.password.value;
-         
-          try {
-            await signInWithEmailAndPassword(auth, correo, contraseña);
-            navigate("/");
-            console.log("Iniciado sesion con exito!!")
-          } catch (error) {
-            alert ("El correo o la contraseña es incorrecta");
-          }
-        
-    }
-  
-    const signInWithGoogle = () => {
-      const provider = new GoogleAuthProvider();
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          console.log(result.user);  
-          navigate("/");
-        }).catch((error) => {
-          console.log(error.message);
-        });
-    };
-    /* Codigo de github para despues
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
-    const signInWithGithub = () => {
-        const provider = new GithubAuthProvider();
-        signInWithPopup(auth, provider)
-          .then((result) => {
-            console.log(result.user);  // Puedes acceder a la información del usuario aquí
-            navigate("/Home");
-          }).catch((error) => {
-            console.log(error.message);
-          });
-      };
-    */
-  
-      return (
-        <div className='d-flex d-flex justify-content-center align-items-center'>
-        <div className="formulario2">
+
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const functAutenticacion = async (e) => {
+    e.preventDefault();
+    const correo = e.target.email.value;
+    const contraseña = e.target.password.value;
+
+    try {
+      await signInWithEmailAndPassword(auth, correo, contraseña);
+      navigate("/");
+      console.log("Iniciado sesion con exito!!")
+    } catch (error) {
+      alert("El correo o la contraseña es incorrecta");
+    }
+
+  }
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        navigate("/");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+
+  const signInWithGithub = () => {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+
+  return (
+    <div className='d-flex d-flex justify-content-center align-items-center'>
+      <div className="formulario2">
         <h2 className='form-title2'>Iniciar Sesion</h2>
-        <hr className='line-separator'/>
+        <hr className='line-separator' />
         <form onSubmit={functAutenticacion}>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">Ingrese email:</label>
-            <input className='input-field' type='text' placeholder='Ingresar email' id='email' required/>
+            <input className='input-field' type='text' placeholder='Ingresar email' id='email' required />
           </div>
-          <hr className='line-separator'/>
+          <hr className='line-separator' />
           <div className="mb-3">
             <label htmlFor="password" className="form-label">Ingrese contraseña:</label>
-            <input className='input-field' type={showPassword ? "text" : "password"} placeholder='Ingresar contraseña ' id='password' required/>
+            <input className='input-field' type={showPassword ? "text" : "password"} placeholder='Ingresar contraseña ' id='password' required />
           </div>
-          
+
           <button className='boton'>Inicia Sesion</button>
         </form>
-  
-        <button onClick={signInWithGoogle} className='google-signin-button'>
-          <FontAwesomeIcon icon={faGoogle}  className='iconoG'/>
-          
-        </button>  
-
+        <div className='ContenedorBtn'>
+          <button onClick={signInWithGoogle} className='google-signin-button'>
+            <FontAwesomeIcon icon={faGoogle} className='iconoG' />
+          </button>
+          <button onClick={signInWithGithub} className='GitHub-signin-button'>
+            <FontAwesomeIcon icon={faGithub} className='iconoG' />
+          </button>
+        </div>
       </div>
-      </div>
-    );
+    </div>
+  );
 }
-      export default Login;
+export default Login;
