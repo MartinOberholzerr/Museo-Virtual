f4178f5a0973b8bb9a529aaed73073067394888a
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-10-18 22:26:35 -0300

    export loading

--- ap/src/components/loading.js
+++ ap/src/components/loading.js
@@ -2,33 +2,38 @@ import React from 'react';
 import './loading.css';
 import ('https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css');
 
-<div class="book">
-  <div class="inner">
-    <div class="left"></div>
-    <div class="middle"></div>
-    <div class="right"></div>
-  </div>
-  <ul>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-  </ul>
+function Loading(){
+    <div class="book">
+      <div class="inner">
+        <div class="left"></div>
+        <div class="middle"></div>
+        <div class="right"></div>
+      </div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
 
-</div>
-
+    </div>
+  return(
+    <div>hola</div>
+  )
+};
   
+export default Loading
