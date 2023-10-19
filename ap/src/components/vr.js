53a36c73cb09c67b7850606144d079e0267b7305
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-10-18 22:52:21 -0300

    Empezando con el VR 360

---
+++ ap/src/components/vr.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './vr.css';
+
+function Vr (){
+    
+return(
+    <iframe src='https://momento360.com/e/u/1bb8634342d848d6b68fb1737fc543a0?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true'></iframe>
+)}
+
+
+export default Vr
