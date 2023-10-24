a1a6bb43629f734a42c7e44bd58dc59a6cfb4390
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-10-24 19:01:52 -0300

    cambios, funciona vr

--- ap/src/App.js
+++ ap/src/App.js
@@ -1,31 +1,18 @@
 import React, { useState } from "react";
 import Loading from "./components/loading";
-//import Vr from "./components/vr";
-//import Moving from "./components/move";
+import Moving from "./components/move";
+import Vr from "./components/vr";
 
 
-function App (){
-    const [loading, setLoading] = useState(false);
+function App(){
 
-    const cambiarEstado=()=>{
-      setLoading(true);
-      setTimeout(()=>{
-        setLoading(false);
-      }, 5000);
-    }
-  if(Loading){
-    return(
-      <Loading></Loading>
-    )
-  }
-  else{
   return(
-    <div>
-      <button onClick={()=>cambiarEstado()}>Reload</button>
-    </div>
-  );
-}
-}
+    <>
+    <Vr></Vr>
+    <Moving></Moving>
+    </>
+  
+)}
 
 
 
--- ap/src/components/vr.js
+++ ap/src/components/vr.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './vr.css';
 
 function Vr(){
-    const display = {
-        display: 'none',
-      };
+    <div>
+        
+        
+    </div>
 return(
     <div>
-        <iframe id="iframe1" className='vr'  src='https://momento360.com/e/u/1bb8634342d848d6b68fb1737fc543a0?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true'></iframe>
-        <iframe id="iframe2" className='vr' src="https://momento360.com/e/u/0104bc85af4b41e382a6430c71166279?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"></iframe>
+        <iframe id="iframe1" className='vr'  src='https://momento360.com/e/u/a194b404d22649f0b45bfce8a932d458?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true'></iframe>
     </div>
     )}
-    
-
       
 export default Vr
