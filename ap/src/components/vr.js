d60b054623f86550e3fb8bac62b6c5a11ee2cde9
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-11-11 22:08:43 -0300

    -

--- ap/src/components/vr.js
+++ ap/src/components/vr.js
@@ -9,6 +9,7 @@ function Vr(){
 return(
     <div>
         <iframe id="iframe1" className='vr'  src='https://momento360.com/e/u/a194b404d22649f0b45bfce8a932d458?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true'></iframe>
+        
     </div>
     )}
       
