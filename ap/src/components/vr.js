660a24fc69e86d7c8217493d6a59fe7cdabcf318
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-11-11 23:25:23 -0300

    resolviendo problemas de ancho

--- ap/src/components/vr.js
+++ ap/src/components/vr.js
@@ -4,17 +4,21 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Vr() {
   const containerStyle = {
-    width: '100%', // Establecer el ancho al 100% de la pantalla
-    height: '100vh', // Establecer la altura al 100% de la pantalla
+    width: '100%',
+    height: '100vh',
     display: 'flex',
-    alignItems: 'center', // Centrar verticalmente el contenido
-    justifyContent: 'center', // Centrar horizontalmente el contenido
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  const imageStyle = {
+    height: '80vh', // Ajusta el valor según tus necesidades para aumentar la altura de las imágenes
   };
 
   return (
     <div style={containerStyle}>
       <Carousel
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: '80%' }}
         infiniteLoop
         autoPlay
         showThumbs={false}
@@ -25,6 +29,7 @@ function Vr() {
             title="VR 1"
             className="vr"
             src="https://momento360.com/e/u/a194b404d22649f0b45bfce8a932d458?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+            style={imageStyle}
           ></iframe>
         </div>
         <div>
@@ -32,6 +37,7 @@ function Vr() {
             title="VR 2"
             className="vr"
             src="https://momento360.com/e/u/0897fe54100b4566b74745167a58f403?utm_campaign=embed&utm_source=other&heading=108.87&pitch=-6.04&field-of-view=75&size=medium&display-plan=true"
+            style={imageStyle}
           ></iframe>
         </div>
         <div>
@@ -39,6 +45,7 @@ function Vr() {
             title="VR 3"
             className="vr"
             src="https://momento360.com/e/u/6c2a1951988244a58e16d3be28e4e00c?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+            style={imageStyle}
           ></iframe>
         </div>
         <div>
@@ -46,6 +53,7 @@ function Vr() {
             title="VR 4"
             className="vr"
             src="https://momento360.com/e/u/3f92948890d048e6a88a281c3c0583f2?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+            style={imageStyle}
           ></iframe>
         </div>
         <div>
@@ -53,6 +61,7 @@ function Vr() {
             title="VR 5"
             className="vr"
             src="https://momento360.com/e/u/db23c8c5cfca4236912fe4e4d389b710?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+            style={imageStyle}
           ></iframe>
         </div>
       </Carousel>
